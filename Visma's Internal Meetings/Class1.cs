a88ba595ca973e43096d5fbc860dfc409f9fc376
Dangis Bakutis <38555777+suneither@users.noreply.github.com>
2022-05-27 12:21:22 +0300

    Add project files.

---
+++ Visma's Internal Meetings/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visma_s_Internal_Meetings
+{
+    internal class Class1
+    {
+        
+    }
+}
